d8c7a64afe9e8132f5df6dda69b8a0afbf501b4d
Sergey Petra <sergpetra@gmail.com>
2017-03-22 02:20:39 +0300

    новая задачка про сон овечки

---
+++ CountingSheep/CountingSheep/BleatrixTrotterSheepAlgo.cs
@@ -0,0 +1,48 @@
+﻿namespace CountingSheep
+{
+    //https://code.google.com/codejam/contest/6254486/dashboard
+    public class BleatrixTrotterSheepAlgo
+    {
+        private const int Max = 1022;
+        private int _current = 0;
+        private int _next = 0;
+
+        public BleatrixTrotterSheepAlgo(int n)
+        {
+            int limit = 0, j = 2;
+
+            StoreDigits(n);
+            
+            while (limit++ < 100 && _current != Max)
+            {
+                _next = n * j;
+                StoreDigits(_next);
+                j++;
+            }
+
+            _next = -1; //INSOMNIA
+        }
+
+        private void StoreDigits(int next)
+        {
+            
+            while (true)
+            {
+                if (next < 10)
+                {
+                    _current = _current | 2 << (next - 1);
+                    return;
+                }
+
+                int m = (next / 10);
+                var t = next - m * 10;
+                _current = _current | 2 << (t - 1);
+
+                next = m;
+            }
+            
+        }
+
+        public int Result => _next;
+    }
+}
---
+++ CountingSheep/CountingSheep/UnitTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.Remoting.Messaging;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CountingSheep
+{
+    public class UnitTests
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public UnitTests(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        //https://code.google.com/codejam/contest/6254486/dashboard
+
+        [Theory]
+        [InlineData(11, 99)]
+        [InlineData(1692, 5076)]
+        [InlineData(2, 90)]
+        public void aaa(int n, int answer)
+        {
+            BleatrixTrotterSheepAlgo a = new BleatrixTrotterSheepAlgo(n);
+            Assert.Equal(a.Result, answer);
+        }
+            
+    }
+}
